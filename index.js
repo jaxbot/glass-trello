80b3084d9db048cdbb5296b7d8a97a568ed94836
Jonathan Warner <jaxbot@gmail.com>
2014-04-12 15:57:56 -0400

    Update for latest Prism compatibility

--- index.js
+++ index.js
@@ -18,14 +18,12 @@ var config = require("./config.json");
 // init with key and token from Trello
 var trello = new Trello(config.trello_key, config.trello_token);
 
-config.callbacks = {
-	newclient: onNewClient,
-	subscription: onSubscription
-};
-
 prism.init(config, function(err) {
 	getShoppingList();
 });
+
+prism.on('newclient', onNewClient);
+prism.on('subscription', onSubscription);
 
 function onNewClient(tokens) {
 	getShoppingList();
@@ -48,12 +46,14 @@ function onSubscription(err, payload) {
 };
 
 function getShoppingList() {
+	//prism.deleteBundle({ "bundleId": "trelloshop" });
+
 	trello.get("/1/lists/" + config.lists["shopping"] + "/cards", function(err,data) {
 		var html = prism.cards.main(data);
-		prism.updateAllCards({ html: html, pinned: true, sourceItemId: "trello_cover", bundleId: "trelloshop", isBundleCover: true });
+		prism.all.updateCard({ html: html, pinned: true, sourceItemId: "trello_cover", bundleId: "trelloshop", isBundleCover: true });
 		for (var i = 0; i < data.length; i++) {
 			var html = prism.cards.listitem(data[i]);
-			prism.updateAllCards({ html: html, pinned: true, sourceItemId: "trello_" + data[i].id, bundleId: "trelloshop", isBundleCover: false });
+			prism.all.updateCard({ html: html, pinned: true, sourceItemId: "trello_" + data[i].id, bundleId: "trelloshop", isBundleCover: false });
 		}
 	});
 };
